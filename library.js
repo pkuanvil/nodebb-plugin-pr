b839a8dd51831c89fe0885de3d6dc0f705c2e4bb
pkuanvil-uid1 <119569118+pkuanvil-uid1@users.noreply.github.com>
2023-02-01 18:08:10 +0800

    register: add dkim uuid check

--- library.js
+++ library.js
@@ -92,26 +92,48 @@ plugin.static.user.loggedOut = async (params) => {
 };
 
 plugin.filter.register.check = async (payload) => {
-	const { userData } = payload;
-	if (userData.noscript === 'true') {
-		throw new Error('Registeration requires JavaScript.');
-	}
-	const regreq = `${userData.username}\n${userData.password}`;
-	if (!await db.isSetMember('pr:regreq', regreq)) {
-		throw new Error('The Server has not received your register request.');
-	} else if (await db.isSetMember('pr:regreq_done', regreq)) {
-		throw new Error('This register request has already been completed.');
+	const { noscript, uuid, username, password } = payload.userData;
+	if (uuid) {
+		const uuidStatus = await db.getObject(`pr:dkim:uuid:${uuid}`);
+		if (!uuidStatus || !uuidStatus.status) {
+			throw new Error('Invalid register UUID');
+		}
+		if (uuidStatus.status === 'success') {
+			// Do nothing
+		} else if (uuidStatus.status === 'pending') {
+			throw new Error('Your DKIM register request is waiting for admin approval. Please try again later');
+		} else if (uuidStatus.status === 'done') {
+			throw new Error('Your DKIM register request has already been completed.');
+		} else if (uuidStatus.status === 'rejected') {
+			throw new Error(`Your DKIM register request is rejected, reason: ${uuidStatus.reason}`);
+		} else {
+			throw new Error('Internal Server Error');
+		}
+	} else {
+		if (noscript === 'true') {
+			throw new Error('Registeration requires JavaScript.');
+		}
+		const regreq = `${username}\n${password}`;
+		if (!await db.isSetMember('pr:regreq', regreq)) {
+			throw new Error('The Server has not received your register request.');
+		} else if (await db.isSetMember('pr:regreq_done', regreq)) {
+			throw new Error('This register request has already been completed.');
+		}
 	}
 };
 
 plugin.action.pr_register.abort = async (payload) => {
-	const { req } = payload;
-	const userData = req.session.registration;
-	if (!userData) {
-		return;
+	const { uuid, username, password } = payload.req.session.registration;
+	if (uuid) {
+		const uuidStatus = await db.getObject(`pr:dkim:uuid:${uuid}`);
+		if (uuidStatus.status === 'done') {
+			uuidStatus.status = 'success';
+			await db.setObject(`pr:dkim:uuid:${uuid}`, uuidStatus);
+		}
+	} else {
+		const regreq = `${username}\n${password}`;
+		await db.setRemove('pr:regreq_done', regreq);
 	}
-	const regreq = `${userData.username}\n${userData.password}`;
-	await db.setRemove('pr:regreq_done', regreq);
 };
 
 plugin.filter.sanitize.config = Privacy.sanitizeHTML;
@@ -123,11 +145,20 @@ plugin.filter.register.interstitial = async (payload) => {
 	if (req.method !== 'POST') {
 		return payload;
 	}
+	const { uuid, username, password } = userData;
 	// Don't activate when user POST at first page /register, when user has not yet read the "complete" page warnings
 	// Use route path instead of absolute path, because website can be prefixed
 	if (req.route.path === '/register/complete') {
-		const regreq = `${userData.username}\n${userData.password}`;
-		await db.setAdd('pr:regreq_done', regreq);
+		if (uuid) {
+			const uuidStatus = await db.getObject(`pr:dkim:uuid:${uuid}`);
+			if (uuidStatus.status !== 'done') {
+				uuidStatus.status = 'done';
+				await db.setObject(`pr:dkim:uuid:${uuid}`, uuidStatus);
+			}
+		} else {
+			const regreq = `${username}\n${password}`;
+			await db.setAdd('pr:regreq_done', regreq);
+		}
 	}
 	return payload;
 };
