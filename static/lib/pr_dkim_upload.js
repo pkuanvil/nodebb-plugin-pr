2ea582f68961e384e7654a833f05f67a90ce0d18
pkuanvil-uid1 <119569118+pkuanvil-uid1@users.noreply.github.com>
2023-02-02 13:47:23 +0800

    pr_dkim_upload: add ajax script

--- static/lib/pr_dkim_upload.js
+++ static/lib/pr_dkim_upload.js
@@ -6,11 +6,42 @@
 	It is not bundled into the min file that is served on the first load of the page.
 */
 
-define('forum/pr_dkim_upload', function () {
-	var module = {};
-	module.init = function () {
-		// $('#last-p').text('pr_dkim_upload.js loaded!');
-		console.log('pr_dkim_upload.js loaded');
+define('forum/pr_dkim_upload', [
+	'translator',
+], function (translator) {
+	const pr_dkim_upload = {};
+	pr_dkim_upload.init = function () {
+		const submit = $('#upload-submit');
+		const errorEl = $('#upload-error-notify');
+		submit.on('click', function (e) {
+			e.preventDefault();
+			errorEl.addClass('hidden');
+			submit.parents('form').ajaxSubmit({
+				dataType: 'json',
+				success: function (data) {
+					if (!data) {
+						return;
+					}
+					if (data.next) {
+						const pathname = utils.urlToLocation(data.next).pathname;
+
+						const params = utils.params({ url: data.next });
+						params.registered = true;
+						const qs = decodeURIComponent($.param(params));
+
+						window.location.href = pathname + '?' + qs;
+					}
+				},
+				error: function (data) {
+					const status = data.responseJSON.status;
+					const message = status.message || data.responseText;
+					translator.translate(message, config.defaultLang, function (translated) {
+						errorEl.find('p').text(translated);
+						errorEl.removeClass('hidden');
+					});
+				},
+			});
+		});
 	};
-	return module;
+	return pr_dkim_upload;
 });
--- static/templates/pr_dkim_upload.tpl
+++ static/templates/pr_dkim_upload.tpl
@@ -1,9 +1,12 @@
+<div class="alert alert-danger<!-- IF !error --> hidden<!-- ENDIF !error -->" id="upload-error-notify" >
+  <p>{error}</p>
+</div>
 <form method="post" enctype="multipart/form-data">
   <div>
     <label for="file">[[pr:upload.file]]</label>
     <input type="file" id="file" name="file" multiple="">
   </div>
   <div>
-    <button>[[pr:upload.submit]]</button>
+    <button id="upload-submit">[[pr:upload.submit]]</button>
   </div>
 </form>
